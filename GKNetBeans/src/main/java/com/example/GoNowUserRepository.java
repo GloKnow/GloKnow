047f2c5b02814e5131e63e8c2fbab9faa23fdcfa
Yskinator <yski2moreletters@gmail.com>
2016-03-19 10:56:29 +0200

    Re-did the entire project based on two example projects to finally get rid of all the errors.

--- GKNetBeans/src/main/java/com/example/GoNowUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-//import com.example.GoNowUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GoNowUserRepository extends JpaRepository<GoNowUser, Long> {
-    
-}
