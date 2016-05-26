ccf13b384bd8c3ae30d29c6cd3fea705bcea7bc9
Yskinator <yski2moreletters@gmail.com>
2016-05-26 12:46:08 +0300

    Added a single default user to prod profile.

--- GoNow/src/main/java/wad/profiles/ProdProfile.java
+++ GoNow/src/main/java/wad/profiles/ProdProfile.java
@@ -2,8 +2,10 @@ package wad.profiles;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,11 +14,37 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import wad.domain.Person;
+import wad.repository.ActivityRepository;
+import wad.repository.PersonRepository;
+import wad.repository.PostRepository;
 
 @Configuration
 @Profile("prod")
 public class ProdProfile {
+    @Autowired
+    private PersonRepository personRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+    
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @PostConstruct
+    public void init() {
+        Person admin = new Person();
+        admin.setFirstname("A");
+        admin.setSurname("Dmin");
+        admin.setBirthday("Today");
+        admin.setEmail("asdasdasd@asd.asd");
+        admin.setPhone("040-toolazy");
+        admin.setUsername("kljhasklyhf934quqfyqlifliyvja");
+        admin.setPassword("kljhasklyhf934quqfyqlifliyvja");
+
+        admin = personRepository.save(admin);
+    }
+    
     @Bean
     public PlatformTransactionManager transactionManager() throws URISyntaxException {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
