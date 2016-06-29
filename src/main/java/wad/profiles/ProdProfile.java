fd5695fe64de49e9fb506fc4c7dd41e19a4677f5
Yskinator <yski2moreletters@gmail.com>
2016-06-29 18:13:24 +0300

    Possibly fixed prod profile.

--- src/main/java/wad/profiles/ProdProfile.java
+++ src/main/java/wad/profiles/ProdProfile.java
@@ -45,44 +45,5 @@ public class ProdProfile {
         admin = personRepository.save(admin);
     }
     
-    @Bean
-    public PlatformTransactionManager transactionManager() throws URISyntaxException {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws URISyntaxException {
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaDialect(new HibernateJpaDialect());
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
-
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPersistenceUnitName("production");
-        factory.setPackagesToScan("wad.domain");
-        factory.setDataSource(dataSource());
-
-        factory.afterPropertiesSet();
-        return factory;
-    }
-
-    @Bean
-    public DataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName("org.postgresql.Driver");
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
-    }
 }
